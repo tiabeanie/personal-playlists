6046bb807900a4f7faee1efb0fa4f246aecbe351
tiabeanie <sydney.jane.hunter@gmail.com>
2020-11-12 10:46:14 -0700

    added routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :playlists
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace "api" do
+    namespace "v1" do
+      resources :sessions, only: [:create]
+      delete :logout, to: "sessions#logout"
+      get :logged_in, to: "sessions#logged_in"
+
+      resources :users, only: [:create] do
+        resources :snippet_categories do
+          resources :snippets
+        end
+      end
+    end
+  end
 end

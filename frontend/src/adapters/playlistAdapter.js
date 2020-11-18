dca584271301d2c53b46ed766a6e62ccb03c6c08
tiabeanie <sydney.jane.hunter@gmail.com>
2020-11-18 10:13:55 -0700

    added playlist adapter

---
+++ frontend/src/adapters/playlistAdapter.js
@@ -0,0 +1,31 @@
+class PlaylistAdapter {
+    constructor(userId) {
+      this.userId = userId
+    }
+  
+    async getPlaylists() {
+      return fetch(`${app.baseUrl}users/${this.userId}/playlists`)
+               .then(result => result.json())
+               .catch(error => error.message)
+    }
+  
+    async createPlaylist(title, owner) {
+      const playlistData = {
+        title: title,
+        owner: owner
+      }
+  
+      const configObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(playlistData)
+      }
+  
+      return fetch(`${app.baseUrl}users/${this.userId}/playlists`, configObject)
+              .then(result => result.json())
+              .catch(error => error.message)
+    }
+  }
